7fb43bc16e9f4488359f1a0f1d40524d2173bf3f
Fernando Rojo <fernandosrojo@yahoo.com>
2020-05-07 14:50:18 -0400

    add firebase auth to Fuego

--- src/classes/Fuego.ts
+++ src/classes/Fuego.ts
@@ -1,12 +1,17 @@
 import * as firebase from 'firebase'
 
+import 'firebase/firestore'
+import 'firebase/auth'
+
 type Config = Parameters<typeof firebase.initializeApp>[0]
 
 export class Fuego {
   public db: ReturnType<firebase.app.App['firestore']>
+  public auth: typeof firebase.auth
   constructor(config: Config) {
     this.db = !firebase.apps.length
       ? firebase.initializeApp(config).firestore()
       : firebase.app().firestore()
+    this.auth = firebase.auth
   }
 }
