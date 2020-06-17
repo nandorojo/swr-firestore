e6468c145a6af7f3a9a5873f13ccf0aeb42b72c2
nandorojo <frojo@sas.upenn.edu>
2020-06-17 15:59:15 -0400

    🙉remove comments

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,15 +1,9 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useContext } from 'react'
 import { createContext, ReactNode } from 'react'
 import { Fuego } from '../classes/Fuego'
 
-// @ts-ignore
 type Context = {
   fuego: Fuego
-  // listeners: {
-  // 	[name: string]: null | Listener
-  // }
-  // addListener: (name: string, listener: Listener) => void
-  // unsubscribeListener: (name: string) => void
 }
 
 // @ts-ignore
@@ -24,63 +18,13 @@ export let fuego: Fuego
 
 export const setFuego = (f: Fuego) => (fuego = f)
 
-// type Listener = {
-//   unsubscribe: ReturnType<firebase.firestore.DocumentReference['onSnapshot']>
-//   updateData: <T>(docOrCollection: T) => void
-// }
-
 export const useFuegoContext = () => {
   const context = useContext(FuegoContext)
   return context
 }
 
 export const FuegoProvider = ({ children, fuego }: Props) => {
-  // const [listeners, setListeners] = useState<Context['listeners']>({})
-
   setFuego(fuego)
-  // useEffect(() => {
-  //   fuego = f
-  // }, [f])
-
-  // const listeners = useRef<{
-  // 	[name: string]: null | {
-  // 		listener: ReturnType<
-  // 			firebase.firestore.DocumentReference['onSnapshot']
-  // 		>
-  // 		updateData: (docOrCollection: any) => {}
-  // 	}
-  // }>({})
-
-  // const addListener = useCallback(
-  // 	(name: string, listener: Listener) => {
-  // 		setListeners(state => {
-  // 			if (state[name]) {
-  // 				console.warn(
-  // 					`Fuego listener error. Tried to add ${name} listener, but it already exists. This still worked, but using redux might be a better solution.`
-  // 				)
-  // 			}
-  // 			if (
-  // 				Object.entries(listeners).filter(
-  // 					([_, listener]) => listener
-  // 				).length > 4
-  // 			) {
-  // 				console.warn(
-  // 					`You have ${Object.keys(
-  // 						listeners
-  // 					)} listener(s) open. You may want to unsubscribe. Open: ${Object.keys(
-  // 						listeners
-  // 					)}`
-  // 				)
-  // 			}
-  // 			return { ...state, [name]: listener }
-  // 		})
-  // 	},
-  // 	[listeners]
-  // )
-  // const unsubscribeListener = useCallback((name: string) => {
-  // 	setListeners(state => ({ ...state, [name]: null }))
-  // }, [])
-
   return (
     <FuegoContext.Provider value={{ fuego }}>{children}</FuegoContext.Provider>
   )
