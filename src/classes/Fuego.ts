6e0ea4e6246e140d47175b7943720ffc0a6c2206
Al Pagan <generamics@gmail.com>
2020-06-30 10:47:09 +0100

    Add support for Firebase functions

--- src/classes/Fuego.ts
+++ src/classes/Fuego.ts
@@ -2,16 +2,19 @@ import * as firebase from 'firebase/app'
 
 // import 'firebase/firestore'
 // import 'firebase/auth'
+// import 'firebase/functions'
 
 type Config = Parameters<typeof firebase.initializeApp>[0]
 
 export class Fuego {
   public db: ReturnType<firebase.app.App['firestore']>
   public auth: typeof firebase.auth
+  public functions: typeof firebase.functions
   constructor(config: Config) {
     this.db = !firebase.apps.length
       ? firebase.initializeApp(config).firestore()
       : firebase.app().firestore()
     this.auth = firebase.auth
+    this.functions = firebase.functions
   }
 }
